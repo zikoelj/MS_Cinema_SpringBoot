2f67f0c36f15a9bcaeba49f04eea48ec9689ba62
zikoelj <zakariaelj24@gmail.com>
2025-01-01 01:22:21 +0100

    removing customer and account directories

--- account-service/src/main/java/net/eljabiry/accountservice/enums/AccountType.java
+++
@@ -1,6 +0,0 @@
-package net.eljabiry.accountservice.enums;
-
-public enum AccountType {
-   CURRENT_ACCOUNT ,
-    SAVING_ACCOUNT
-}
--- customer-service/src/main/java/net/eljabiry/customerservice/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package net.eljabiry.customerservice.repository;
-
-import net.eljabiry.customerservice.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
-}
