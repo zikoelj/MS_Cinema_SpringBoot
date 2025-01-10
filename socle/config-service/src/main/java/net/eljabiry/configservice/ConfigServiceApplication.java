66962a1c77fdbc2309d9ad5b3ea7e73e162702e6
zikoelj <zakariaelj24@gmail.com>
2025-01-10 16:41:03 +0100

    good

--- socle/config-service/src/main/java/net/eljabiry/configservice/ConfigServiceApplication.java
+++ socle/config-service/src/main/java/net/eljabiry/configservice/ConfigServiceApplication.java
@@ -2,12 +2,12 @@ package net.eljabiry.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConfigServiceApplication.class, args);
     }
-
 }
